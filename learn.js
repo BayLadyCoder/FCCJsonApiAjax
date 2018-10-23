20ed8401e06716e511c9bd2f8add0f69885ee88d
BayLadyCoder <rachada_32@hotmail.com>
2018-10-22 22:51:51 -0400

    Convert JSON Data to HTML

--- learn.js
+++ learn.js
@@ -138,3 +138,67 @@ document.addEventListener('DOMContentLoaded', function () {
         };
     };
 });
+
+
+
+
+/*
+JSON APIs and Ajax: Convert JSON Data to HTML
+
+Now that you're getting data from a JSON API, you can display it in the HTML.
+
+You can use a forEach method to loop through the data since the cat photo objects are held in an array. As you get to each item, you can modify the HTML elements.
+
+First, declare an html variable with var html = "";.
+
+Then, loop through the JSON, adding HTML to the variable that wraps the key names in strong tags, followed by the value. When the loop is finished, you render it.
+
+Here's the code that does this:
+
+json.forEach(function(val) {
+  var keys = Object.keys(val);
+  html += "<div class = 'cat'>";
+  keys.forEach(function(key) {
+    html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
+  });
+  html += "</div><br>";
+});
+
+Add a forEach method to loop over the JSON data and create the HTML elements to display it.
+
+Here is some example JSON
+
+[
+  {
+    "id":0,
+      "imageLink":"https://s3.amazonaws.com/freecodecamp/funny-cat.jpg",
+      "altText":"A white cat wearing a green helmet shaped melon on its head. ",
+      "codeNames":[ "Juggernaut", "Mrs. Wallace", "Buttercup"
+    ]
+  }
+]
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('getMessage').onclick = function () {
+        req = new XMLHttpRequest();
+        req.open("GET", '/json/cats.json', true);
+        req.send();
+        req.onload = function () {
+            json = JSON.parse(req.responseText);
+            var html = "";
+            // Add your code below this line
+            json.forEach(function (val) {
+                var keys = Object.keys(val);
+                html += "<div class = 'cat'>";
+                keys.forEach(function (key) {
+                    html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
+                });
+                html += "</div><br>";
+            });
+
+
+            // Add your code above this line
+            document.getElementsByClassName('message')[0].innerHTML = html;
+        };
+    };
+});
