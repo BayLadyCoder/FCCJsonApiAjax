4beb05d89f0599b4a26469f7a7db8c864ce3421a
BayLadyCoder <rachada_32@hotmail.com>
2018-10-22 22:53:27 -0400

    Render Images from Data Sources

--- learn.js
+++ learn.js
@@ -202,3 +202,38 @@ document.addEventListener('DOMContentLoaded', function () {
         };
     };
 });
+
+/*
+JSON APIs and Ajax: Render Images from Data Sources
+
+The last few challenges showed that each object in the JSON array contains an imageLink key with a value that is the URL of a cat's image.
+
+When you're looping through these objects, you can use this imageLink property to display this image in an img element.
+
+Here's the code that does this:
+
+html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+
+
+Add code to use the imageLink and altText properties in an img tag.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('getMessage').onclick = function () {
+        req = new XMLHttpRequest();
+        req.open("GET", '/json/cats.json', true);
+        req.send();
+        req.onload = function () {
+            json = JSON.parse(req.responseText);
+            var html = "";
+            json.forEach(function (val) {
+                html += "<div class = 'cat'>";
+                // Add your code below this line
+
+                html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+                // Add your code above this line
+                html += "</div><br>";
+            });
+            document.getElementsByClassName('message')[0].innerHTML = html;
+        };
+    };
+});
